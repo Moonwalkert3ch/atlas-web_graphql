60349545d7a6bfeff057b36438b272bb41fc0f4d
Moonwalkert3ch <6620@holbertonstudents.com>
2024-07-08 22:13:06 -0500

    connect to mongodb Atlas and created a mongoose model

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 const schema = require("./schema/schema");
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://atlas-sql-667d7f97689b541727735f00-bzpst.a.query.mongodb.net/sample_mflix?ssl=true&authSource=admin';
 
 const app = express();
 
-app.use('/graphql',graphqlHTTP({
-    schema: schema,
-    graphiql: true, // Enables GraphiQL UI for testing
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true, // Enables GraphiQL UI for testing
 })
 );
 
-app.listen(4000,()=>{
+app.listen(4000, () => {
   console.log('now listening for request on port 4000');
 });
